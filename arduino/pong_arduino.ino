2e6a8dbce4611291ac0dafee2cad418a75e9b91c
MatyAlts <matiass.torres@alumnos.frm.utn.edu.ar>
2023-11-08 16:59:52 -0300

    version 1.0.0

---
+++ arduino/pong_arduino.ino
@@ -0,0 +1,274 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd (2,3,4,5,6,7);
+
+const int MIN_PALETA_IA_Y = 10;   // Posición mínima de la paleta de la IA
+const int MAX_PALETA_IA_Y = 400;  // Posición máxima de la paleta de la IA
+
+class Player{
+  private:
+  int Y_POS = 210;
+  int SCORE = 0;
+  public:
+  void limits(){
+    Y_POS = constrain(Y_POS, MIN_PALETA_IA_Y, MAX_PALETA_IA_Y);
+  }
+  void changePos(int y){
+    Y_POS = y;
+  }
+  void changeScore(){
+    SCORE++;
+  }
+  void resetScore(){
+    SCORE = 0;
+  }
+  void sumPos(int y){
+    Y_POS += y;
+  }
+  void equalsPos(int y){
+    Y_POS = y;
+  }
+  int PosValue(){
+    return Y_POS;
+  }
+  int ScoreValue(){
+    return SCORE;
+  }
+};
+Player Player_1, Player_2;
+
+char* diff = "hard";
+bool start = false;
+int potPin = A0;       // El pin analógico donde está conectado el potenciómetro
+/*Player_1.changePos(210);    // Posición Y inicial de la paleta 1
+Player_2.changePos(210);    // Posición Y inicial de la paleta 2*/
+int pelotaX = 480;     // Posición X inicial de la pelota
+int pelotaY = 240;     // Posición Y inicial de la pelota
+int direccionPelotaX = random(2) == 0 ? -5 : -5; // Dirección X inicial de la pelota
+int direccionPelotaY = random(2) == 0 ? -5 : 5; // Dirección Y inicial de la pelota
+int puntaje_1 = 0;    //Puntaje inicial del jugador 1
+int puntaje_2 = 0;    //Puntaje inicial del jugador 2
+int recording = false;
+int replaying = false;
+
+const int RANGO_PALETA_IA_MIN = 150; // Posición mínima del rango de movimiento de la paleta de la IA
+const int RANGO_PALETA_IA_MAX = 330; // Posición máxima del rango de movimiento de la paleta de la IA
+
+const int VELOCIDAD_PALETA_IA = 8; // Velocidad de movimiento de la paleta de la IA
+
+unsigned long ultimoCambioDireccion = 0; // Almacena el tiempo del último cambio de dirección
+unsigned long intervaloCambioDireccion = 800; // Intervalo de tiempo para cambiar la dirección (2 segundos)
+
+int direccionActual = 1; // Dirección de movimiento actual (1 para arriba, -1 para abajo)
+
+int dificultad = 5;   //Dificultad de la IA
+
+
+void seguirPelota(){
+  // Calcula la diferencia entre la posición de la paleta de la IA y la posición de la pelota
+  int diferencia = pelotaY - Player_2.PosValue();
+
+  // Ajusta la posición de la paleta de la IA en función de la diferencia
+  if (abs(diferencia) > VELOCIDAD_PALETA_IA) {
+    if (diferencia > 0) {
+      Player_2.sumPos(VELOCIDAD_PALETA_IA);
+    } else if (diferencia < 20) {
+      Player_2.sumPos(-VELOCIDAD_PALETA_IA);
+    }
+  } else {
+    // Si la diferencia es menor que la velocidad de la paleta, mueve la paleta directamente a la posición de la pelota
+    Player_2.equalsPos(pelotaY);
+  }
+}
+
+void movimientoAleatorio() {
+  unsigned long tiempoActual = millis();
+
+  if (tiempoActual - ultimoCambioDireccion >= intervaloCambioDireccion) {
+    // Cambia la dirección de forma cíclica cada intervaloCambioDireccion
+    direccionActual = -direccionActual; // Cambia la dirección (de arriba a abajo o viceversa)
+
+    // Actualiza el tiempo del último cambio de dirección
+    ultimoCambioDireccion = tiempoActual;
+  }
+
+  // Mueve la paleta de la IA en la dirección actual
+  Player_2.sumPos(direccionActual * VELOCIDAD_PALETA_IA);
+}
+
+void setup() {
+  analogWrite(11,70);    //Establece el brillo de la pantalla LCD
+  lcd.begin(16,2);    //Inicializa el LCD
+  pinMode(13, OUTPUT);    //Pin para el LED
+  Serial.begin(19200);    //Velocidad del serial
+  randomSeed(analogRead(0));    // Inicializa la semilla aleatoria
+  Serial.println("Done!");    //Primer mensaje al iniciar Arduino
+}
+
+void loop() {
+  Player_2.limits();
+  if(!replaying){
+    lcd.clear();
+    lcd.setCursor(0,0);     //Especifica al LCD donde debe escribir
+    lcd.print("Player 1:");
+    lcd.setCursor(14,0);
+    lcd.print(Player_1.ScoreValue());
+    lcd.setCursor(0,1);
+    lcd.print("CPU:");
+    lcd.setCursor(5,1);
+    lcd.print(diff);      //Imprime la dificultad de la CPU
+    lcd.setCursor(14,1);
+    lcd.print(Player_2.ScoreValue());
+    if(recording){
+      delay(1);
+      lcd.setCursor(10,0);
+      lcd.print("REC");
+    }
+  }
+  else{
+    dificultad = 5;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Reproduciendo");
+    lcd.setCursor(0,1);
+    lcd.print("Partida");
+    lcd.setCursor(7,1);
+    lcd.print(".");
+  }
+
+  /******************************************************************************************/
+  //Leer los mensajes recibidos por Serial
+
+  String mensaje = ""; // Inicializa una cadena vacía
+
+    // Lee caracteres hasta que se reciba un salto de línea ('\n') o hasta que se alcance un límite máximo
+    char caracter;
+    int maxLongitud = 64; // Establece un límite máximo de longitud del mensaje
+    int tiempoMaximoEspera = 5; // Establece un tiempo máximo de espera en milisegundos
+
+    unsigned long inicioTiempo = millis();
+    while (millis() - inicioTiempo < tiempoMaximoEspera) {
+      if (Serial.available() > 0) {
+        caracter = Serial.read();
+        mensaje += caracter;
+        if (caracter == '\n' || mensaje.length() >= maxLongitud) {
+          // Se encontró un salto de línea o se alcanzó el límite máximo
+          break;
+        }
+      }
+    }
+
+    // Procesa el mensaje si no está vacío
+    if (mensaje.length() > 0) {
+      // Verifica si el mensaje recibido es igual a "reinicio"
+      if (mensaje.equals("reinicio\n")) {
+        // Si el mensaje es igual a "reinicio", reinicia el puntaje de los jugadores
+        Player_1.resetScore();
+        Player_2.resetScore();
+      }
+      if (mensaje.equals("recON\n")) {
+        recording = true;
+      }
+      if (mensaje.equals("recOFF\n")) {
+        recording = false;
+      }
+      if (mensaje.equals("replayON\n")) {
+        replaying = true;
+      }
+      if (mensaje.equals("replayOFF\n")) {
+        replaying = false;
+      }
+      // Verifica si el mensaje recibido es igual a "start"
+      if (mensaje.equals("start\n")) {
+        // Si el mensaje es igual a "start", empieza a funcionar el juego
+    start = true;
+      }
+      // Verifica si el mensaje recibido es igual a "dificil"
+      if (mensaje.equals("dificil\n")) {
+        // Si el mensaje es igual a "dificil", cambia la dificultad del juego
+        diff = "hard";
+        dificultad = 5;
+      }
+      // Verifica si el mensaje recibido es igual a "facil"
+      if (mensaje.equals("facil\n")) {
+        // Si el mensaje es igual a "facil", cambia la dificultad del juego
+        diff = "easy";
+        dificultad = 20;
+      }
+    }
+    /**************************************************************************************************/
+  //El juego solo se ejecuta si el usuario lo pide
+  if(start){
+    if(!replaying){
+
+  // Lee el valor del potenciómetro  
+  int valorPot = analogRead(potPin);
+
+  // Mapea el valor del potenciómetro al rango de la pantalla (10 a 420)
+  Player_1.changePos(map(valorPot, 0, 1023, 10, 420));
+
+  if (pelotaX > 480) {
+    // La pelota está más allá del límite, sigue la configuración de seguimiento
+    seguirPelota();
+  } else {
+    // La pelota está antes del límite, permite el movimiento aleatorio
+    movimientoAleatorio();
+  }
+
+  // Actualizar la posición de la pelota
+  pelotaX += direccionPelotaX;
+  pelotaY += direccionPelotaY;
+
+  // Detectar colisiones con las paletas (simulada)
+  if (pelotaX < 40 && pelotaY > Player_1.PosValue() && pelotaY < Player_1.PosValue() + 60) {
+    direccionPelotaX = 5;
+  } else if (pelotaX > 920 && pelotaY > Player_2.PosValue() && pelotaY < Player_2.PosValue() + 60) {
+    direccionPelotaX = -5;
+  }
+
+  // Detectar colisiones con los bordes superior e inferior
+  if (pelotaY < 10 || pelotaY > 470) {
+    direccionPelotaY *= -1;
+  }
+
+  // Detectar puntuación
+  if (pelotaX < 0) {
+    // Puntuación para jugador 2
+    pelotaX = 480; // Reiniciar la posición de la pelota
+    pelotaY = 240;
+    direccionPelotaX = random(2) == 0 ? -5 : 5; // Cambiar la dirección X de la pelota
+    direccionPelotaY = random(2) == 0 ? -5 : 5; // Cambiar la dirección Y de la pelota
+    Player_2.changeScore();
+    digitalWrite(13,HIGH);
+    delay(100);
+    digitalWrite(13,LOW);
+  } else if (pelotaX > 960) {
+    // Puntuación para jugador 1
+    pelotaX = 480; // Reiniciar la posición de la pelota
+    pelotaY = 240;
+    direccionPelotaX = random(2) == 0 ? -5 : 5; // Cambiar la dirección X de la pelota
+    direccionPelotaY = random(2) == 0 ? -5 : 5; // Cambiar la dirección Y de la pelota
+    Player_1.changeScore();
+    digitalWrite(13,HIGH);
+    delay(100);
+    digitalWrite(13,LOW);
+  }
+  // Envía la posición de la pelota y las paletas por serial a Processing
+  Serial.print(Player_1.PosValue());
+  Serial.print(",");
+  Serial.print(Player_2.PosValue());
+  Serial.print(",");
+  Serial.print(pelotaX);
+  Serial.print(",");
+  Serial.print(pelotaY);
+  Serial.print(",");
+  Serial.print(direccionPelotaX);
+  Serial.print(",");
+  Serial.print(direccionPelotaY);
+  Serial.print(",");
+  Serial.print(Player_1.ScoreValue());
+  Serial.print(",");
+  Serial.println(Player_2.ScoreValue());
+  }
+}
+  delay(dificultad);
+}
---
+++ processing/pong_processing.pde
@@ -0,0 +1,284 @@
+import processing.serial.*;
+import controlP5.*;
+
+ControlP5 cp5;
+Textfield miCampoDeTexto;
+boolean borrarRectangulo = false;
+boolean easyMode = false;
+boolean hardMode = true;
+ArrayList<String> datosGuardados = new ArrayList<String>();
+boolean grabando = false;
+boolean reproduciendo = false;
+boolean mostrarTexto = false;
+PImage icono;
+PImage fondo;
+
+String[] datosReplay;
+int indiceReplay = 0;
+PrintWriter writer; // Declarar un objeto PrintWriter
+
+//Completar con el nombre del puerto serial
+String Puerto = "COM6";
+boolean Start = false;
+
+Serial puertoSerial;  // Objeto Serial para la comunicación con Arduino
+int paleta1Y, paleta2Y, pelotaX, pelotaY, direccionPelotaX, direccionPelotaY, puntaje_1, puntaje_2;
+
+void setup() {
+  fondo = loadImage("background.png");
+  icono = loadImage("icon.png");
+  surface.setIcon(icono);
+  println(Serial.list()); // Imprime los puertos disponibles
+  size(960, 480);  // Tamaño de la ventana
+  background(fondo);   // Fondo negro
+  textAlign(CENTER, CENTER);
+  cp5 = new ControlP5(this);
+  PFont fuente = createFont("Arial", 16);
+  // Crea un campo de texto
+  miCampoDeTexto = cp5.addTextfield("dificultad")
+                     .setPosition(width/2-100, 400)
+                     .setSize(200, 40)
+                     .setColorBackground(color(255, 255, 255))
+                     .setColorValue(color(0, 0, 0))
+                     .setFont(fuente);
+  
+  // Configuración del puerto serial
+  try {
+    // Intenta abrir el puerto serial
+    puertoSerial = new Serial(this, Puerto, 19200);
+    println("Puerto serial abierto con éxito.");
+  } catch (RuntimeException e) {
+    // Maneja la excepción si no se puede abrir el puerto serial
+    println("Error al abrir el puerto serial: " + e.getMessage());
+  }
+  // Inicializa las variables
+  paleta1Y = 210;
+  paleta2Y = 210;
+  pelotaX = 480;
+  pelotaY = 240;
+  direccionPelotaX = 1;
+  direccionPelotaY = 1;
+  puntaje_1 = 0;
+  puntaje_2 = 0;
+  
+  frameRate(60);  // Establece la velocidad de fotogramas a 60 FPS
+}
+void draw() {
+  background(fondo);
+  
+  cp5.draw();
+  if (borrarRectangulo) {
+    fill(240); // Cambia el color del fondo al color de fondo actual
+    rect(20, 20, 200, 40); // Dibuja un rectángulo sobre el campo de texto
+    borrarRectangulo = false; // Reinicia la bandera para evitar borrar continuamente
+  }
+  if(mostrarTexto){
+    textSize(32);
+    text("Pulsa 4 para seguir jugando.",width/2, height/2);
+  }
+  // Lee datos del puerto serial si están disponibles
+  if(!reproduciendo){
+    while (puertoSerial.available() > 0) {
+      String datos = puertoSerial.readStringUntil('\n');
+        if (datos != null) {
+          if(grabando){
+            datosGuardados.add(datos); // Agrega datos a la lista
+          }
+        String[] valores = datos.trim().split(",");
+          if (valores.length == 8) {
+            paleta1Y = int(valores[0]);
+            paleta2Y = int(valores[1]);
+            pelotaX = int(valores[2]);
+            pelotaY = int(valores[3]);
+            direccionPelotaX = int(valores[4]);
+            direccionPelotaY = int(valores[5]);
+            puntaje_1 = int(valores[6]);
+            puntaje_2 = int(valores[7]);
+          }
+       }
+    }
+  }
+  else{ // Lee datos directamente desde el archivo guardado
+    frameRate(120);
+    if (indiceReplay < datosReplay.length) {
+    String datos = datosReplay[indiceReplay];
+    String[] valores = datos.split(",");
+    if (datos != null) {
+    if (valores.length == 8) {
+      paleta1Y = int(valores[0]);
+      paleta2Y = int(valores[1]);
+      pelotaX = int(valores[2]);
+      pelotaY = int(valores[3]);
+      direccionPelotaX = int(valores[4]);
+      direccionPelotaY = int(valores[5]);
+      puntaje_1 = int(valores[6]);
+      puntaje_2 = int(valores[7]);
+    }
+  }
+  }
+  else{
+    mostrarTexto = true; // Muestra la pantalla cuando finalice la repeticion
+    return;
+  }
+  indiceReplay++;
+  }
+  // Limpia la pantalla
+  background(fondo);
+  textSize(64);
+  if(Start == true){
+    textSize(12);
+    text("dificultad",width/2,30);
+    text("facil",width/2,50);
+    if(easyMode){
+      text("x",width/2 + 20,50);
+    }
+    text("dificil",width/2,70);
+    if(hardMode){
+      text("x",width/2 + 20,70);
+    }
+    
+    textSize(64);
+  // Dibuja las paletas
+  fill(255,0,0);
+  rect(20, paleta1Y, 20, 60); // Paleta 1
+  fill(0,0,255);
+  rect(920, paleta2Y, 20, 60); // Paleta 2
+  
+  // Dibuja la pelota
+  fill(0,255,0);
+  ellipse(pelotaX, pelotaY, 20, 20);
+  
+  fill(255,0,0);
+  text(puntaje_1, 380, 50);
+  fill(0,0,255);
+  text(puntaje_2, 580, 50);
+  fill(255);
+  if(reproduciendo){
+      textSize(20);
+      text("reproduciendo grabacion..",150,30);
+      textSize(64);
+    }
+  if(grabando){
+      textSize(20);
+      text("grabando repeticion..",150,30);
+      textSize(64);
+    }
+  }
+  else{
+    text("Pulsa ENTER para empezar.",width/2, height/2-40);
+    textSize(32);
+    text("Pulsa 1 para grabar, 2 para parar",width/2, height/2+40);
+    text("Pulsa 3 para cargar la repeticion",width/2, height/2+80);
+  }
+}
+void keyPressed(){
+  String resetGame = "reinicio";
+  byte[] resetGameAction = resetGame.getBytes();
+  if (key == '3' && Start == true) {
+    easyMode = false;
+    hardMode = true;
+    cargarReplay("replay.txt");
+    reproduciendo = true;
+    String replayON = "replayON";
+    byte[] replayONSer = replayON.getBytes();
+    puertoSerial.write('\n');
+    puertoSerial.write(replayONSer);
+    puertoSerial.write('\n');
+  }
+  if (key == '4' && Start == true) {
+    reproduciendo = false;
+    puertoSerial.write('\n');
+    puertoSerial.write(resetGameAction);
+    puertoSerial.write('\n');
+    String replayOFF = "replayOFF";
+    byte[] replayOFFSer = replayOFF.getBytes();
+    puertoSerial.write('\n');
+    puertoSerial.write(replayOFFSer);
+    puertoSerial.write('\n');
+    String hard = "dificil";
+    byte[] hardDif = hard.getBytes();
+    println("Dificultad cambiada a: DIFICIL");
+    easyMode = false;
+    hardMode = true;
+    puertoSerial.write('\n');
+    puertoSerial.write(hardDif);
+    puertoSerial.write('\n');
+    indiceReplay = 0;
+  }
+  if (key == '1' && Start == true) {
+    // Comienza a grabar
+    grabando = true;
+    String recON = "recON";
+    byte[] recONSer = recON.getBytes();
+    puertoSerial.write('\n');
+    puertoSerial.write(recONSer);
+    puertoSerial.write('\n');
+  } else if (key == '2' && Start == true) {
+    String recOFF = "recOFF";
+    byte[] recOFFSer = recOFF.getBytes();
+    puertoSerial.write('\n');
+    puertoSerial.write(recOFFSer);
+    puertoSerial.write('\n');
+    // Detiene la grabación y guarda los datos en un archivo
+    grabando = false;
+    guardarReplay("replay.txt");
+  }
+  if(key == 'r' || key == 'R' && Start == true) {
+    puertoSerial.write('\n');
+    puertoSerial.write(resetGameAction);
+    puertoSerial.write('\n');
+  }
+  String startGame = "start";
+  byte[] startGameAction = startGame.getBytes();
+  if(key==10){
+    String textoIngresado = miCampoDeTexto.getText();
+    
+    // Haz algo con el texto ingresado, por ejemplo, imprímelo
+    if(textoIngresado.toUpperCase().equals("FACIL")){
+      String easy = "facil";
+      byte[] easyDif = easy.getBytes();
+      println("Dificultad cambiada a: FACIL");
+      hardMode = false;
+      easyMode = true;
+      puertoSerial.write('\n');
+      puertoSerial.write(easyDif);
+      puertoSerial.write('\n');
+    }
+    if(textoIngresado.toUpperCase().equals("DIFICIL")){
+      String hard = "dificil";
+      byte[] hardDif = hard.getBytes();
+      println("Dificultad cambiada a: DIFICIL");
+      easyMode = false;
+      hardMode = true;
+      puertoSerial.write('\n');
+      puertoSerial.write(hardDif);
+      puertoSerial.write('\n');
+    }
+    // Borra el contenido del campo de texto
+    miCampoDeTexto.clear();
+    
+    // Marca la bandera para borrar el rectángulo
+    borrarRectangulo = true;
+    
+    puertoSerial.write('\n');
+    puertoSerial.write(startGameAction);
+    puertoSerial.write('\n');
+    Start = true;
+    }
+}
+void guardarReplay(String nombreArchivo) {
+  writer = createWriter(nombreArchivo); // Crear un objeto PrintWriter
+  for (String dato : datosGuardados) {
+    if (!dato.trim().isEmpty()) { // Verifica que el dato no esté vacío
+      writer.println(dato); // Escribe el dato en una línea
+    }
+  }
+  writer.flush(); // Vacía el búfer del escritor
+  writer.close(); // Cierra el archivo
+  println("Replay guardado en " + nombreArchivo);
+  datosGuardados.clear(); // Limpia la lista de datos grabados
+}
+void cargarReplay(String nombreArchivo) {
+  datosReplay = loadStrings(nombreArchivo);
+  println("Replay cargado desde " + nombreArchivo);
+}
